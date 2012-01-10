ee2c576e921b9a59ef7e9fcdabd73b96f580f160
Elisabeth Baseman <ebaseman@gmail.com>
2012-01-10 12:35:10 -0500

    Fixed simple unification

--- src/gist/unifier.clj
+++ src/gist/unifier.clj
@@ -4,22 +4,26 @@
   gist.unifier
   (:use [gist.lang]))
 
-(defn bound?
-      [v, m]
-      ((contains? m v) true false))
+(defn isbound?
+      [m, v]
+      (contains? m v))
 
 (defn bind
       [v, t, m]
       (assoc m v t))
 
+(def unify-args)
+
 (defn unify
   [x, y, m]
-  (cond
-   (and (op? x) (op? y)) (if (sameop x y) (unify (args x) (args y) m) false)
-   (and (iconst? x) (iconst? y)) (if (= x y) m false)
-   (and (fconst? x) (fconst? y)) (if (= x y) m false)
-   (isvar?   x) (if (bound? x m) (unify (get m x) y) (bind x y m))
+  (cond  
+   (and (is-iconst? x) (is-iconst? y)) (if (= x y) m false)
+   (and (is-fconst? x) (is-fconst? y)) (if (= x y) m false)
+   (is-varn?   x) (if (isbound? m x) (unify (get m x) y) (bind x y m))
+   (and (op? x) (op? y)) (if (sameop x y) (unify-args (args x) (args y) m) false)
    :default false))
+
+;;need to go through and unify each args
 
 
 
