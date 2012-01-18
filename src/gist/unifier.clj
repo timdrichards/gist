e837af0ce127c5e881495f101876f39203a0b961
Elisabeth Baseman <ebaseman@gmail.com>
2012-01-18 16:00:13 -0500

    Updated unifier

--- src/gist/unifier.clj
+++ src/gist/unifier.clj
@@ -12,18 +12,34 @@
       [v, t, m]
       (assoc m v t))
 
+(defn is-empty?
+      [x]
+      (== (count x) 0))
+
 (def unify-args)
 
 (defn unify
-  [x, y, m]
+  [x y m]
+ ;; (println x "," y "," m)
   (cond  
+   ;;(and (is-empty? x) (is-empty? y)) m
    (and (is-iconst? x) (is-iconst? y)) (if (= x y) m false)
    (and (is-fconst? x) (is-fconst? y)) (if (= x y) m false)
    (is-varn?   x) (if (isbound? m x) (unify (get m x) y) (bind x y m))
    (and (op? x) (op? y)) (if (sameop x y) (unify-args (args x) (args y) m) false)
    :default false))
 
-;;need to go through and unify each args
+(defn unify-args
+      [xargs yargs m]
+     ;; (println "xargs:" xargs)
+      (cond
+	(not (== (count xargs) (count yargs))) false
+	(== (count xargs) 0) m
+	:default (let [b (unify (first xargs) (first yargs) m)] 
+          (if (not b)
+	      false
+	      (unify-args (rest xargs) (rest yargs) b)))))
+      
 
 
 
